925d1348729f0fed5f9759125133bda7c325e2ca
Artur <57113088+KsyDark@users.noreply.github.com>
2020-05-29 01:05:42 +0300

    Add files via upload

---
+++ Расчёт объёма спирта/com.bat
@@ -0,0 +1,5 @@
+:: ô ØçãÆ ¨å ÆÖÒØÐ·¤·œ ØáÖ¨Æå  ã ØÖÒÖùíœ pyinstaller
+pyinstaller --onefile --noconsole --icon=alc.ico "â ãû„å Ö¢ž„Ò  ãØ·áå .py"
+:: ô ØçãÆ ¨Ò ØáÖ¬á ÒÒç ØÖãÐ¨ ÆÖÒØ·ÐÞ¤··
+cd dist
+start "â ãû„å" "â ãû„å Ö¢ž„Ò  ãØ·áå .exe""
